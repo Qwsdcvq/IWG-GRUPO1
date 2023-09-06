67f09babdb50914e74c6d63fd8bca096c91a4826
joaquin <joaguilarzapata@gmail.com>
2023-09-05 22:44:04 -0300

    probando

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Intro',
+    'Intro.apps.SocialConfig',
 ]
 
 MIDDLEWARE = [
