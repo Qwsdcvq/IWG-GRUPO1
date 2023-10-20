74dad8b443a55db43d270a1385e0c9bbbb89a301
joaquin <joaguilarzapata@gmail.com>
2023-10-20 01:04:42 -0300

    Añadi una capa al mapa, faltan marcadores dinamicos

--- Intro/static/js/mapa.js
+++ Intro/static/js/mapa.js
@@ -1,17 +1,29 @@
+
 var map = L.map('map').fitWorld();
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+var osmH = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+
+})
+
+L.control.layers({
+    "OpenStreetMap": osm,
+    "OpenStreetMap.HOT": osmH
+}).addTo(map);
+osm.addTo(map);
+
 
 map.locate({setView: true, maxZoom: 16});
 
 function onLocationFound(e) {
-    var radius = Math.trunc(e.accuracy);
+    var radius = e.accuracy;
 
     L.marker(e.latlng).addTo(map)
-        .bindPopup("Estas a " + radius + " metros de este punto").openPopup();
+        .bindPopup("Estas a " + Math.trunc(radius) + " metros de este punto").openPopup();
 
     L.circle(e.latlng, radius).addTo(map);
 }
